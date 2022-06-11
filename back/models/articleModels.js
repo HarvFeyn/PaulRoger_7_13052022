c36385f8d2ee842bf1ac2f6e09efbe189c3fe917
Feyn <pauger.rol@gmail.com>
2022-06-11 16:12:59 +0200

    PDW-60 PDW-61

--- back/models/articleModels.js
+++ back/models/articleModels.js
@@ -1,7 +1,7 @@
 const query = require("../helpers/dbpool").query
 
 exports.getOneArticle = (id) => {
-    return query("SELECT * FROM article WHERE id = ?", [id])
+    return query("SELECT * FROM article WHERE id = '" + id + "'")
         .then(result => {
             return result
         })
@@ -18,6 +18,22 @@ exports.creatArticle = (article) => {
     return query("INSERT INTO article (title, text, author, likes, date) VALUES (?, ?, ?, ?, ?)", [article.title, article.text, article.author, "", new Date()])
         .then(() => {
             console.log('article created')
+            return {message: "ok"}
+        })
+};
+
+exports.deleteArtcile = (id) => {
+    return query("DELETE FROM article WHERE id = '" + id + "'")
+        .then(() => {
+            console.log('article deleted')
+            return {message: "ok"}
+        })
+};
+
+exports.modifyArticle = (id, article) => {
+    return query("UPDATE article SET (title, text) VALUES (?, ?) WHERE id = '" + id + "'", [article.title, article.text])
+        .then(() => {
+            console.log('article modify')
             return {message: "ok"}
         })
 };
