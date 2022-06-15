b435d335f9e2769640989b52f35163791a0e358e
Feyn <pauger.rol@gmail.com>
2022-06-15 10:08:56 +0200

    Modif de la modal article PDW-53

--- front/src/components/article/modal/article.vue
+++ front/src/components/article/modal/article.vue
@@ -1,37 +1,55 @@
 <template>
     <div id="article">
-        <p>Create new article:</p>
-        <form @submit="create" class="form-register">
-            <div class="form-register">
-                <label for="title">Title: </label>
-                <input type="text" name="title" id="title" required>
+        <form id="form-article-setting" class="needs-validation">
+            <h2 class="mb-3">{{ localization.addArticle }}</h2>
+            <div class="form-floating mb-3">
+                <input id="floating-name" v-model="title" type="text" class="form-control">
+                <label for="floating-name">Titre :</label>
             </div>
-            <div class="form-register">
-                <label for="body">Write your article here: </label>
-                <input type="text" name="body" id="body" required>
+            <div class="row mb-3 vue-editor">
+                <vue-editor id="vue-editor" v-model="content" use-custom-image-handler :editor-toolbar="customToolbar" placeholder="Saisissez le contenu de l'article ici"/>
             </div>
-            <div class="row mb-3">
-                <button type="submit" class="btn btn-primary">Create Article</button>
-                <p>{{ messageSubmit }}</p>
+            <div class="row mb-3 form-footer">
+                <button id="btn-register-submit" type="submit" class="btn btn-primary">{{ localization.save }}</button>
             </div>
         </form>
     </div>
 </template>
 
 <script>
-const article = require('../../API/article')
+import { VueEditor } from 'vue2-editor'
+const apiArticle = require('../../API/article')
+const localization = require('../../../helpers/localization')
 
     export default {
         name: 'Article',
+        component: {
+            VueEditor
+        },
         data() {
-            return {
-                messageSubmit: ''
-            }
+            return this.initialData()
         },
         methods: {
+            initialData () {
+                return {
+                    localization,
+                    title: undefined,
+                    nameValid: false,
+                    content: '',
+                    contentValid: false,
+                    isValid: false,
+                    confirmation: false,
+                    errorApiMessage: undefined,
+                    customToolbar: [
+                    ['bold', 'italic', 'underline'],
+                    [{ list: 'ordered' }, { list: 'bullet' }],
+                    ['image', 'code-block']
+                    ]
+                }
+            },
             create (event) {
                 event.preventDefault()
-                article.createArticle(event.target.elements.title.value, event.target.elements.body.value, this.$store.state.user.name, this.$store.state.user.token)    
+                apiArticle.createArticle(event.target.elements.title.value, event.target.elements.body.value, this.$store.state.user.name, this.$store.state.user.token)    
                     .then(() => {
                         console.log("article created")
                     })
---
+++ front/src/helpers/localization.js
@@ -0,0 +1,7 @@
+const addArticle = 'Créer un nouvel article'
+const save = 'sauvegarder'
+
+module.exports = {
+  addArticle,
+  save
+}
